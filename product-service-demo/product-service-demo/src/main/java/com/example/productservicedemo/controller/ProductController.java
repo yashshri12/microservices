7b4c5ec4db6dcdd55f27239342e0b8eb1bdf7703
yashshri <yashshrivastav@gamil.com>
2022-04-26 13:31:22 -0700

    api-gateway

--- product-service-demo/product-service-demo/src/main/java/com/example/productservicedemo/controller/ProductController.java
+++ product-service-demo/product-service-demo/src/main/java/com/example/productservicedemo/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ProductController {
@@ -21,5 +23,30 @@ public class ProductController {
     @GetMapping("/name")
     public String getproduct(){
         return "product";
+    }
+
+    @GetMapping("/all")
+    public List<Product> getAll(){
+        return productrepo.findAll();
+    }
+
+    @GetMapping("/get/{id}")
+    public Product getOne(@PathVariable("id") Long id){
+        return  productrepo.findById(id).get();
+    }
+
+    @PutMapping("/update/{id}")
+    public Product updateProduct(@PathVariable("id") Long id,@RequestBody Product product){
+        Product product1=productrepo.findById(id).get();
+
+        product1.setName(product.getName());
+
+
+        return productrepo.save(product1);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteproduct(@PathVariable("id") Long id){
+        productrepo.deleteById(id);
     }
 }
